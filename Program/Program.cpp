8c784b4686a9eb62051fe1ffe7011e63f2b9cc02
jinjae84 <jinjae84@naver.com>
2024-12-13 21:43:52 +0900

    (2024.12.13) 선택 정렬

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,39 +1,58 @@
 ﻿#include <iostream>
 
-#define SIZE 5
-
 using namespace std;
+
+int Euclid(int x, int y)
+{
+	// 1. 두 개의 자연수 x와 y가 있습니다.
+	//    그리고 x와 y로 나눈 나머지를 R이라고 합니다.
+	int R = x % y;
+	// 2. R이 0이라면, x와 y의 최대공약수가 y가 됩니다.
+	if (R == 0)
+	{
+		return y;
+	}
+	// 3. R이 0이 아니라면, x와 y의 최대공약수는 y와 R의
+	//    최대공약수와 같기 때문에 x에는 y 그리고 y에는 R을
+	//    대입한 후 1번으로 돌아갑니다.
+	else if (R != 0)
+	{
+		return Euclid(y, R);
+	}	
+}
 
 int main()
 {
-#pragma region 선택 정렬
-	// 주어진 리스트 중에 최소값을 찾아서 맨 앞에 위치한
-	// 결과를 교체하는 방식으로 정렬하는 알고리즘입니다.
+#pragma region 유클리드 호제법
+	// 2개의 자연수 또는 정수의 최대 공약수를 구하는 방식으로
+	// 두 수가 서로 상대방 수를 나누어서 원하는 수를 얻어내는 알고리즘입니다.
 
-	int list[SIZE] = {5,13,9,1,2};
+	// int x = 0;
+	// 
+	// int y = 0;
+	// 
+	// int result = 0;
+	// 
+	// cin >> x >> y;
+	// 
+	// for (int i = 1; i <= x && i <= y; i++)
+	// {
+	// 	if (x % i == 0 && y % i == 0)
+	// 	{
+	// 		result = i;
+	// 	}
+	// }
+	// 
+	// cout << "최대 공약수 : " << result << endl;
 
-	for (int i = 0; i < SIZE - 1; i++)
-	{
-		int index = i;
-		for (int j = i + 1; j < SIZE; j++)
-		{
-			if (list[j] < list[index])
-			{
-				index = j;
-			}
-		}
+	int x = 0;
+	int y = 0;
 
-		if (list[i] > list[index])
-		{
-			std::swap(list[i], list[index]);
-		}
-	}
+	cin >> x >> y;
 
-	for (const int& element : list)
-	{
-		cout << element << " ";
-	}
+	int result = Euclid(x, y);
 
+	cout << "최대 공약수 : " << result << endl;
 #pragma endregion
 
 
